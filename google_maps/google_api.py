25628b43767d18b9457df677e40c32853c6cb055
Mitchell Aaron Chandra <mitchachsn@Mitchells-MacBook-Pro-2.local>
2020-02-29 17:30:47 +0000

    syntax fix

--- google_maps/google_api.py
+++ google_maps/google_api.py
@@ -6,6 +6,6 @@ API_KEY = 'AIzaSyCV-wrawCG8i8cmnmVoecV4M72l1afcjXM'
 
 gmaps = googlemaps.Client(key=API_KEY)
 
-places_result = gmaps.places_nearby(location='55.944734, -3.187243', radius=40000, open_now = False, type='airport')
+places_result = gmaps.places_nearby(location='55.944734, -3.187243', radius=40000, open_now=False, type='airport')
 
 pprint.pprint(places_result)
