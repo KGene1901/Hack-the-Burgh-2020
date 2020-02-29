d39c5c232ee45980909ce100d8ef2160735eaee4
Kah Gene Leong <kgleong2001@gmail.com>
2020-02-29 21:10:29 +0000

    Updated points system code

--- points system.py
+++ points system.py
@@ -3,18 +3,23 @@ import random
 
 details = {'OriDest':'LHR', 'InitDest':'HKG', 'AltDest':'DXB'} # sample location
 
-CF = [{'item':'Beef', 'amt':36.4}, {'item':'Cheese', 'amt':13.5}, {'item':'Chicken', 'amt':6.9}, {'item':'Black Coffee', 'amt':0.021}, {'item':'Dairy Milk', 'amt':1.4}]
+CF = [{'item':'beef', 'amt':36.4}, {'item':'cheese', 'amt':13.5}, {'item':'chicken', 'amt':6.9}, {'item':'black coffee', 'amt':0.021}, {'item':'dairy milk', 'amt':1.4}, {'item':'wood', 'amt':0.4}]
 
 headers = {'api-key': 'hack-the-burgh-2020',}
 
 
-def compareCF():
+def compareCF(CO2_saved):
 
     n = random.randint(0,5)
     item = CF[n]['item']
     amt = CF[n]['amt']
 
-    print(item, amt)
+    num = round(CO2_saved/amt, 2)
+
+    if item == 'wood':
+        print('Thats about {} woods burned!'.format(num))
+    else:
+        print('Thats about {} {} consumed!'.format(num,item))
 
 
 def scoring(score):
@@ -35,12 +40,13 @@ def compareCO2(headers, details):
     CO2_saved = r[1]['emissions'] - r[0]['emissions']
 
     print("Congratulations, you saved {} kg of CO2 emissions".format(round(CO2_saved, 3)))
+    compareCF(CO2_saved)
 
     return round(scoring(CO2_saved),0)
 
 if __name__ == "__main__":
     bonusPt = compareCO2(headers, details)
-    print(bonusPt)
+    print('Bonus point earned:', bonusPt)
 
         
 
