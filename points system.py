8b50e9d36ddf34709dfe04b2f98c58168a5010f0
Kah Gene Leong <kgleong2001@gmail.com>
2020-02-29 15:28:28 +0000

    Point system code

---
+++ points system.py
@@ -0,0 +1,24 @@
+import requests
+
+details = {'Origin':'EDI', 'IDest':''}
+
+Origin = 'EDI'
+Destination = 'DUB'
+
+headers = {
+    'api-key': 'hack-the-burgh-2020',
+}
+
+params = (
+    ('routes', 'EDI,DUB'),
+)
+
+response = requests.get('https://www.skyscanner.net/g/chiron/api/v1/eco/average-emissions', headers=headers, params=params)
+
+#NB. Original query string below. It seems impossible to parse and
+#reproduce query strings 100% accurately so the one below is given
+#in case the reproduced version is not "correct".
+# response = requests.get('https://www.skyscanner.net/g/chiron/api/v1/eco/average-emissions?routes=EDI,DUB', headers=headers)
+r = response.json()
+print(r[0]['emissions'])
+
