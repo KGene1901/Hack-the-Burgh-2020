699eecc48a8d456a6cf89b0b315e0764713778fa
Mitchell Aaron Chandra <mitchachsn@Mitchells-MacBook-Pro-2.local>
2020-02-29 18:28:15 +0000

    dictionary process

--- google_maps/google_api.py
+++ google_maps/google_api.py
@@ -2,13 +2,15 @@ import googlemaps
 import pprint
 import time
 
-API_KEY = ''
+latlondict = {'25.252777, 55.364445'}
+
+API_KEY = 'AIzaSyCV-wrawCG8i8cmnmVoecV4M72l1afcjXM'
 
 gmaps = googlemaps.Client(key=API_KEY)
 
 places_result = gmaps.places_nearby(location='55.944734, -3.187243', radius=40000, open_now=False, type='airport')
 
-# pprint.pprint(places_result)
+# pprint.pprint(places_result)aq1
 
 # pause the script for 3 seconds
 # time.sleep(3)
