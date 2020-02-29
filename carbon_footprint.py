307b7517cd764a66978a9626aca05df2d9774716
Kah Gene Leong <kgleong2001@gmail.com>
2020-02-29 20:29:35 +0000

    Banana

---
+++ carbon_footprint.py
@@ -0,0 +1,4 @@
+CF = [{'item':'Beef', 'amt':36.4}, {'item':'Cheese', 'amt':13.5}, {'item':'Chicken', 'amt':6.9}, {'item':'Black Coffee', 'amt':0.021}, {'item':'Dairy Milk', 'amt':1.4}]
+
+
+
--- points system.py
+++ points system.py
@@ -1,8 +1,21 @@
 import requests
+import random
 
-details = {'OriDest':'LHR', 'InitDest':'HKG', 'AltDest':'DXB'}
+details = {'OriDest':'LHR', 'InitDest':'HKG', 'AltDest':'DXB'} # sample location
+
+CF = [{'item':'Beef', 'amt':36.4}, {'item':'Cheese', 'amt':13.5}, {'item':'Chicken', 'amt':6.9}, {'item':'Black Coffee', 'amt':0.021}, {'item':'Dairy Milk', 'amt':1.4}]
 
 headers = {'api-key': 'hack-the-burgh-2020',}
+
+
+def compareCF():
+
+    n = random.randint(0,5)
+    item = CF[n]['item']
+    amt = CF[n]['amt']
+
+    print(item, amt)
+
 
 def scoring(score):
 
@@ -12,7 +25,7 @@ def scoring(score):
 
     return bonusPt
 
-def comparePoints(headers, details):
+def compareCO2(headers, details):
     params = (('routes', details['OriDest']+','+details['InitDest']+';'+ details['OriDest']+','+details['AltDest']),)
 
     response = requests.get('https://www.skyscanner.net/g/chiron/api/v1/eco/average-emissions', headers=headers, params=params)
@@ -21,12 +34,12 @@ def comparePoints(headers, details):
 
     CO2_saved = r[1]['emissions'] - r[0]['emissions']
 
-    print("Congratulations, you saved {} CO2 emissions".format(round(CO2_saved, 3)))
+    print("Congratulations, you saved {} kg of CO2 emissions".format(round(CO2_saved, 3)))
 
     return round(scoring(CO2_saved),0)
 
 if __name__ == "__main__":
-    bonusPt = comparePoints(headers, details)
+    bonusPt = compareCO2(headers, details)
     print(bonusPt)
 
         
