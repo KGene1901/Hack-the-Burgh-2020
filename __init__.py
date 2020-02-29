c808aa6bbc5498f272f8fb518ab20de40c15ca69
Irenitemi Agbejule <Irenitemi81@gmail.com>
2020-02-29 22:40:24 +0000

    For Gene

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_login import LoginManage
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -10,4 +10,4 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 
-from app import routes, models
+from models import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,32 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import render_template, flash,redirect
+from forms import LoginForm
+from flask_login import current_user, login_user
+from models import User
 
-@app.route('/')
-def hello():
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY']='h'
+
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect('/index')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route("/")
+@app.route('/index')
+def hell():
     return 'Hello World!'
 
 if __name__ == '__main__':
