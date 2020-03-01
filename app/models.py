394e8a68a08a4e3b611df16e162855b766a46c09
Mitchell Aaron Chandra <mitchachsn@Mitchells-MacBook-Pro-2.local>
2020-03-01 11:10:27 +0000

    HOLY SHIT AMAZING STRATS

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from app import db
-from app import login
+from app.routes import login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
--- app/routes.py
+++ app/routes.py
@@ -3,12 +3,50 @@ from app import app
 from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 from flask_login import current_user, login_user, LoginManager
-from app.models import User
 from flask_login import LoginManager
 
-login = LoginManager(app)
+from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'h'
+login = LoginManager(app)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+class User(UserMixin, db.Model):
+    __table_args__ = {'extend_existing': True}
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    point_total = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+
+class Airport(db.Model):
+    __table_args__ = {'extend_existing': True}
+    Airport_Name = db.Column(db.String(3), primary_key=True)
+    Description = db.Column(db.String)
+    IATA = db.Column(db.String)
+
+    def __repr__(self):
+        return '<Airport {}>'.format(self.Airport_Name)
 
 
 @app.route('/login', methods=['GET', 'POST'])
